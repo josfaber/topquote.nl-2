a4467ee4cc1fa97040f1802284ec5a94b599b56a
Jos <795480+josfaber@users.noreply.github.com>
2022-05-02 14:12:25 +0200

    sitemap

--- app/framework/controller/tools.php
+++ app/framework/controller/tools.php
@@ -305,7 +305,7 @@ class Tools
 		}
 
 		$now = date("Y-m-d H:i:s");
-		$sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL; 
+		$sitemap = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL; 
 		
 		$sitemap .= '<url>' . PHP_EOL;
 		$sitemap .= '	<loc>https://topquote.nl</loc>' . PHP_EOL;
